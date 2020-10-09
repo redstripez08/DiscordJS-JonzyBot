1eff3e3cc523b5e766b5ffcfa3cb3e30a7843f58
Angelo <70970878+redstripez08@users.noreply.github.com>
2020-10-10 00:00:53 +0800

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -7,29 +7,37 @@ module.exports = {
     guildOnly: false,
     args: false,
     role: null,
-    execute(message, args) {
+    async execute(message, args) {
         const fetch = require("node-fetch");
         const { MessageEmbed } = require("discord.js");
 
-        (async() => {
-            try {
-                const link = "https://meme-api.herokuapp.com/gimme";
+        try {
+            const originLink = "https://meme-api.herokuapp.com/gimme";
+            const link = new URL(originLink);
+            const options = {
+                method: "GET",
+                headers: {
+                    "Accept": "application/json"
+                }
+            };
 
-                const data = await fetch(link).then(res => res.json());
-    
-                const memeEmbed = new MessageEmbed()
-                    .setColor("#eb1717")
-                    .setURL(data.postLink)
-                    .setAuthor(`r/${data.subreddit}`)
-                    .setTitle(data.title)
-                    .setImage(data.url)
-                    .setFooter(`Author: ${data.author}`);
-    
-                message.channel.send(memeEmbed);
-    
-            } catch (error) {
-                message.channel.send(`There was an error:\n\`${error}\``)
-            }
-        })();
+            const res = await fetch(link, options);
+            if (!res.ok) throw `HTTP Status Code: ${res.status} || ${res.statusText}`;
+
+            const data = await res.json();
+
+            const memeEmbed = new MessageEmbed()
+                .setColor("#eb1717")
+                .setURL(data.postLink)
+                .setAuthor(`r/${data.subreddit}`)
+                .setTitle(data.title)
+                .setImage(data.url)
+                .setFooter(`Author: ${data.author}`);
+
+            message.channel.send(memeEmbed);
+
+        } catch (error) {
+            message.channel.send(`There was an error:\n\`${error}\``)
+        }
     }
 };
