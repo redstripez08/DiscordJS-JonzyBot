099159602ccc1f4ff4f4af1cfdcf18a86cbb91f3
Angelo <70970878+redstripez08@users.noreply.github.com>
2020-10-10 00:08:49 +0800

    Update fact.js

--- commands/fact.js
+++ commands/fact.js
@@ -7,29 +7,38 @@ module.exports = {
     guildOnly: false,
     args: false,
     role: null,
-    execute(message, args) {
+    async execute(message, args) {
         const fetch = require('node-fetch');
-        console.log(args, args[0]);
-        (async() => {
-            try {
-                if (!args.length) {
-                    const link = "https://uselessfacts.jsph.pl/random.json?language=en";
-                    const res = await fetch(link);
-                    if (res.status < 200 || res.status >= 300) throw `Error Code: ${res.status}`;
-                    const { text } = await res.json();
-                    message.channel.send(text);
-                } else if (args[0].toLowerCase() === 'today') {
-                    const link = "https://uselessfacts.jsph.pl/today.json?language=en";
-                    const res = await fetch(link);
-                    if (res.status < 200 || res.status >= 300) throw `Error Code: ${res.status}`;
-                    const { text } = await res.json();
-                    message.channel.send(text);
-                } else {
-                    message.channel.send("Not a valid argument");
-                } 
-            } catch (error) {
-                message.channel.send(`There was an error:\n\`${error}\``);
-            }
-        })();
+
+        const originLink = "https://uselessfacts.jsph.pl";
+        const query = {language: "en"};
+
+        try {
+            if (!args.length || args[0].toLowerCase() === 'random') {
+                const link = new URL("/random.json", originLink);
+                link.search = new URLSearchParams(query).toString();
+
+                const res = await fetch(link);
+                if (!res.ok) throw `Error Code: ${res.status}`;
+
+                const { text } = await res.json();
+                message.channel.send(text);
+
+            } else if (args[0].toLowerCase() === 'today') {
+                const link = new URL("/today.json", originLink);
+                link.search = new URLSearchParams(query).toString();
+
+                const res = await fetch(link);
+                if (!res.ok) throw `Error Code: ${res.status}`;
+
+                const { text } = await res.json();
+                message.channel.send(text);
+
+            } else {
+                message.channel.send("Not a valid argument");
+            } 
+        } catch (error) {
+            message.channel.send(`There was an error:\n\`${error}\``);
+        }
     }
 };
